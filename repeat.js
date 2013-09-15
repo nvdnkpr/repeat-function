b440575cb8d19a427d788e9d9caa364c52d910ad
Jay R Bolton <jayrbolton@gmail.com>
2013-09-15 10:51:36 -0700

    the passed funciton now takes the index

--- repeat.js
+++ repeat.js
@@ -3,10 +3,10 @@ module.exports = function(limit, val) {
 	var ar = [], push_it;
 
 	if (typeof val === 'function')
-		push_it = function() { ar.push(val()); };
+		push_it = function(i) { ar.push(val(i)); };
 	else
-		push_it = function() { ar.push(val); };
+		push_it = function(i) { ar.push(val); };
 
-	for (var i = 0; i < limit; ++i) push_it();
+	for (var i = 0; i < limit; ++i) push_it(i);
 	return ar;
 };
